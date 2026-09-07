400ddc17c8baf9bda9274281512550d74f166aa1
avdhi23khandelwal <avdhi2006@gmail.com>
2026-09-08 00:28:59 +0530

    Update database.py

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine, Column, Integer, String, Float, DateTime, 
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import random
-
+from datetime import datetime
 DATABASE_URL = "sqlite:///inventory.db"
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
